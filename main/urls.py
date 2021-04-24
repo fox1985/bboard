d85cf31408964fc709e89950fb55f9e1f6cbc534
fox1985 <artemdav2@gmail.com>
2021-04-24 11:42:07 +0600

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from .views import index, other_page, profile, user_activate
+
+from .views import BBLoginView, BBLogoutView, ChangeUserInfoView, BBPasswordChageView, \
+    RegisterUserView, RegisterDoneView, DeleteUserView
+
+app_name = 'main'
+
+urlpatterns = [
+    path('accounts/register/activate/<str:sing>/', user_activate, name='register_activate'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
+    path('accounts/password/change/', BBPasswordChageView.as_view(), name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
+
+]
